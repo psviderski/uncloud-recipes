285be8ee53af3b301e5bf662d0e918104fa08d1f
zombiehoffa <github@quilley.org>
2025-12-28 23:18:44 -0700

    Delete networking/keepalived/checkunifi.sh#!/bin directory

--- networking/keepalived/checkunifi.sh#!/bin/checkunifi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Whatever tests are needed
-# E.g., send request and verify response
-# Check if the container is running
-if docker ps | grep -q 'syncthing-syncthing'; then
-    exit 0
-else
-    exit 1
-fi
